75fa4b405080be4c4369209068d91860041e39c8
Josh Collier <josh@creativeworks.com.au>
2017-09-08 08:37:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import run
 # Run is a file in which I have compiled all the functions that are used and made them subfunctions, as well as making zero send after it runs
 
 def play():
+    run.setup()
     run.zero()
     run.song([3000,2000,20])
 
